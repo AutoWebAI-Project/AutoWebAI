d02efa3f736ad806895c01c126499f69db0e581d
AutoWebAI-Project <mattia.imperiale@hotmail.com>
2025-04-23 18:02:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://autowebai.netlify.app"])
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
