f38e2b652f92a6e674cc17e9ae62ea4c2b2b96bc
AutoWebAI-Project <mattia.imperiale@hotmail.com>
2025-04-23 11:22:48 +0200

    Add files via upload

---
+++ src_App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur AutoWebAI</h1>
+      <p>L’assistant intelligent pour améliorer automatiquement votre site web.</p>
+      <button className="cta">Essayer gratuitement</button>
+    </div>
+  );
+}
+
+export default App;
