679f068679146c56eee89004396b298b1c8840ba
AutoWebAI-Project <mattia.imperiale@hotmail.com>
2025-04-25 02:28:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
           <img
             src="/logo-autowebai.png"
             alt="Logo AutoWebAI"
-            className="h-29"
+            className="h-28"
           />
         </div>
 
